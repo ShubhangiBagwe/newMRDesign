34db48242f96abd2e73df844f1526eb79a48c300
ShubhangiBagwe <shubhangibagwe85@gmail.com>
2025-02-18 17:27:05 +0530

    done frontend diff

---
+++ frontend/js/script.js
@@ -0,0 +1,43 @@
+
+document.getElementById("contactForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const number = document.getElementById("number").value;
+
+
+    try {
+        const response = await fetch(`https://mrdesign.onrender.com/send-email`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, email, message,number }),
+        });
+        if (response.ok) {
+            Swal.fire({
+                icon: "success",
+                title: "Email Sent!",
+                text: "Your email has been sent successfully.",
+                confirmButtonText: "OK",
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Failed to Send",
+                text: "There was a problem sending your email. Please try again later.",
+                confirmButtonText: "OK",
+            });
+        }
+    } catch (error) {
+        console.log('error', error)
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "An unexpected error occurred. Please try again.",
+            confirmButtonText: "OK",
+        });
+    }
+});
